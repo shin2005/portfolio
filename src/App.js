048f9b356fab543542a91a1011305127eceb5b81
Your Name <cieloeste0303@gmail.com>
2019-04-28 21:11:48 +0900

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,18 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Portfolio</h1>
-        <h2>School:</h2>
-        <p>I go to ~~~ school I am first grade ~~~</p>
-        <h2>Recent awards / achievements:</h2>
-        <p>I recently participated in ~~~ </p>
-        <p>I recently became a school president in ~~~ </p>
-        <p>My soccer team ~~~ </p>
+        <h2>School: </h2>
+        <p>I go to Yeoksam Middle School I am second grade </p>
+        <h3>Recent awards / achievements: Award in science writing contest</h3>
+        <p>I recently participated in a science contest </p>
+        <p>I recently became a class president in 2018 </p>
         <h2>Favorite Subject:</h2>
-        <p>My favorite subject is</p>
+        <p>My favorite subject is science. </p>
         <h2>What I want to do when I grow up</h2>
-        <p>When I grow up ~~~</p>
+        <p>When I grow up I want to be a software developer</p>
         <h2>Side projects:</h2>
-        <p>when I was 5th grader I ~~</p>
+        <p>when I was 6th grader I was the best kid in my class.</p>
       </header>
     </div>
   );
