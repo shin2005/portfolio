316b6c46cf254c2bac46e6a9978726f826e88f6d
Your Name <cieloeste0303@gmail.com>
2019-08-25 21:09:38 +0900

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const app = require("./app");
+
+require("greenlock-express")
+  .create({
+    version: "draft-12",
+    configDir: "~/.config/acme",
+    server: "https://acme-v02.api.letsencrypt.org/directory",
+    approveDomains: function approveDomains(opts, certs, cb) {
+      if (certs) {
+        opts.domains = certs.altnames;
+      } else {
+        opts.email = "cieloeste0303@gmail.com";
+        opts.agreeTos = true;
+      }
+
+      cb(null, { options: opts, certs });
+    },
+    app
+  })
+  .listen(80, 443);
