5ebdf8af44acfc0618623e2c6de24f0d276aaf08
Your Name <cieloeste0303@gmail.com>
2019-08-25 21:38:34 +0900

    new files and background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Cover from "./cover.jpg";
+//import Cover from "./cover.jpg";
+//import Cover2 from "./cover2.jpg";
+import Cover3 from "./cover3.jpg";
 import { css } from "emotion";
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactPlayer from "react-player";
@@ -8,7 +10,7 @@ function App() {
   return (
     <div
       className={css`
-        background-image: url("${Cover}");
+        background-image: url("${Cover3}");
         background-size: cover;
         overflow: hidden;
         text-align: center;
