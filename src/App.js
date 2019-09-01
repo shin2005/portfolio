7e96111b75035607baf532ba0ebbfaf9ff6f9b67
Your Name <cieloeste0303@gmail.com>
2019-09-01 21:59:37 +0900

    edits

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from "react";
 import Cover3 from "./cover3.jpg";
 import { css } from "emotion";
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import ReactPlayer from "react-player";
+//import ReactPlayer from "react-player";
+import Intro from "./Intro";
 
 function App() {
   return (
@@ -19,134 +20,7 @@ function App() {
         }
       `}
     >
-      <div style={{ padding: "1rem", color: "#fff" }}>
-        <h1>Portfolio</h1>
-        <h2>School: </h2>
-        <p>I go to Yeoksam Middle School I am second grade </p>
-        <h2>Recent awards / achievements: Award in science writing contest</h2>
-        <p>
-          I recently participated in a science contest. It was a writing contest
-          about the use of{" "}
-          <a href="https://en.wikipedia.org/wiki/Big_data" target="blank">
-            big data
-          </a>
-          , and how it can be of use to humans.There are many uses of big data,
-          because it utilizes massive amounts of information for various
-          services. The data can act as statistics for developers to use.{" "}
-        </p>
-        <p>
-          I recently became a class president in 2018. It was a close vote
-          between 6 kids, but I won by just getting 17 people, just over half.{" "}
-        </p>
-        <h2>Favorite Subject:</h2>
-        <p>
-          My favorite subject is science. I like{" "}
-          <a
-            href="https://en.wikipedia.org/wiki/Quantum_mechanics"
-            target="blank"
-          >
-            quantum mechanics
-          </a>
-          , although it`s true that it`s very hard.Quantum mechanics is the
-          field of science that tries to explain the behavior of particles on
-          the sub-atomic level. Quantum mechanics began in the 20c, when
-          Heisenberg, Schrödinger, Plank, Einstein, and others, announced
-          theories that would contribute to change modern physics forever. It
-          led to the creation of the{" "}
-          <a href="https://en.wikipedia.org/wiki/Standard_Model" target="blank">
-            Standard Model
-          </a>
-          . The Standard Model attempts to explain the basic particles that make
-          up the universe, so as to understand physical phenomenons on the
-          nanoscale(nano=10^-9).{" "}
-          <image src="/Users/anjiyoung/Developer/portfolio/src/Standard Model.svg.png">
-            The Standard Model
-          </image>
-        </p>
-        <h2>Useful sites related to coding</h2>
-        <p>
-          1.
-          <a href="https://github.com/" target="blank">
-            Github
-          </a>
-        </p>
-        <p>
-          2.
-          <a href="https://developer.mozilla.org/en-US/" target="blank">
-            Mozilla Developer Network(MDN)
-          </a>
-        </p>
-        <p>
-          3.
-          <a href="https://stackoverflow.com/" target="blank">
-            Stack Overflow
-          </a>
-        </p>
-        <h2>What I want to do when I grow up</h2>
-        <p>
-          When I grow up I want to be a software developer. One project I
-          thought of was making a website that would host games, but the games
-          would be made by the users. The users could code the games to work on
-          the website(with a little help from the company) and the profit could
-          be shared with the company.{" "}
-        </p>
-        <h2>Side projects:</h2>
-        <p>
-          On Minecraft, I worked on a massive temple above the world. It was
-          made out of every precious metal in the game, and you could make
-          lighting strike at the temple. It could be seen from far away, and it
-          was a great landmark.
-        </p>
-        <h2>Stuff that Interests Me</h2>
-        <p>
-          One of the things that I am most interested in is electronics. What
-          interests me the most is news about Apple because it is my favorite
-          comapany. But these days, I am getting more interested in the changes
-          of the entire 4th Industrial Revolution, ranging from AI to
-          self-driving cars to data privacy. I think that knowing the problems
-          of the sudden changes of the 4th Industrial Revolution is as important
-          as the changes because we will have to fix the changes someday, and
-          its better to change them quickly. So I think that being aware is very
-          important.
-        </p>
-        <h2>My Devices</h2>
-        <p>1. iPhone 6 Gold 16GB</p>
-        <p>2. Airpods 1st generation</p>
-        <p>3. 12.9 inch iPad Pro Space Grey 64GB 3rd generation</p>
-        <p>4. Early 2015 MacBook Pro 13 inch 128GB</p>
-        <p>5. Early 2016 iMac 21.5 inch 1TB</p>
-        <p>6. Logitech G703 Gaming Mouse</p>
-        <p>7. Logitech G512 Carbon Gaming Keyboard</p>
-        <h2>Social</h2>
-        <p>
-          Twitter:{" "}
-          <a href="https://twitter.com/cieloeste0303" target="blank">
-            신동건(@cieloeste0303)
-          </a>
-        </p>
-        <p>
-          Twinkle:
-          <a href="https://www.twinkle.network/users/gunshin" target="blank">
-            gunshin
-          </a>
-          (challenge me to chess on the chat!)
-        </p>
-        <p>
-          Youtube:{" "}
-          <a
-            href="https://www.youtube.com/channel/UCEaSW6TjclcuZzx7c1XUlvg"
-            target="blank"
-          >
-            Dong Gun Shin
-          </a>
-          I have a new video!!{" "}
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=X3hrWGH8Ve8"
-            controls
-            light
-          />
-        </p>
-      </div>
+      <Intro />
     </div>
   );
 }
